8484cd477c59da32170610e213c245fcc23ebf80
Leonardo Moreno <lmoreno@tese.edu.mx>
2021-03-23 13:16:11 -0600

    cambios para agregar los superheroes en lista dinamica

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+
+import {HEROES} from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,15 +10,16 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero ={
-    id: 1,
-    name: 'Spiderman',
-    edad: 23
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero}from "./hero";
+
+export const HEROES: Hero[] = [
+{id: 11, name: "Superman", edad: 30},
+{id: 12, name: "Batman", edad: 30},
+{id: 13, name: "Hulk", edad: 40},
+{id: 14, name: "ironman", edad: 25},
+{id: 15, name: "Thor", edad: 1000},
+{id: 16, name: "Spiderman", edad: 20},
+{id: 17, name: "Black Widow", edad: 28},
+{id: 18, name: "Flash", edad: 28},
+{id: 19, name: "Bruja Escarlata", edad: 36},
+{id: 20, name: "Wonder Woman", edad: 30}
+];
+
